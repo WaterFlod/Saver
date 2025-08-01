f95940077214effcc1c08dfde0c79229d1f6aad1
Shestakov Matvey <mse25019@gmail.com>
2025-08-01 12:16:03 +0500

    add ExpenseModel

---
+++ saver/src/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Annotated
+
+from database import Base
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+
+class ExpenseModel(Base):
+    __tablename__ = "expenses"
+    
+    id: Mapped[intpk]
+    amount: Mapped[int]
+    description: Mapped[str]
