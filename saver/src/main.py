1c16aadc887c4b8828101b8927f1a584d5a4eaf8
Shestakov Matvey <mse25019@gmail.com>
2025-08-01 12:10:07 +0500

    add database.py

--- saver/src/main.py
+++ saver/src/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
-
 import uvicorn
+import asyncio
 
 from routers import router_user 
+from database import Base, engine
 
 app = FastAPI()
 
 app.include_router(router_user)
 
 
+async def init_models():
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+            
+
 if __name__ == "__main__":
+    asyncio.run(init_models())
     uvicorn.run("main:app", reload=True, log_level="info", host="0.0.0.0")
