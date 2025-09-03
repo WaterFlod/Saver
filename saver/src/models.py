6b52e55a31917bb85526c105706493963a759092
Shestakov Matvey <mse25019@gmail.com>
2025-09-03 09:32:17 +0500

    update class Base

--- saver/src/models.py
+++ saver/src/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import text, Integer, func
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, declared_attr
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from typing import Annotated
+from datetime import datetime
 
 from database import Base
 
@@ -8,9 +10,20 @@ int_pk = Annotated[int, mapped_column(primary_key=True), mapped_column(index=Tru
 str_unq = Annotated[str, mapped_column(unique=True)]
 
 
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()[:-5] + 's'
+
+
 class UserModel(Base):
-    __tablename__ = "user"
-    
     __table_args__ = {'extend_existing': True}
     
     id: Mapped[int_pk]
@@ -20,17 +33,13 @@ class UserModel(Base):
     password: Mapped[str]
     
     is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
-    is_student: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
-    is_teacher: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
     is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
-    is_super_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
     
 
 class ExpenseModel(Base):
-    __tablename__ = "expense"
-    
     __table_args__ = {'extend_existing': True}
     
     id: Mapped[int_pk]
+    #user_id: Mapped[int] = mapped_column(foreing_key=UserModel.id)
     amount: Mapped[int]
     description: Mapped[str]
