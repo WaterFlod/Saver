63d251709d03bd92e282fe4601284939279439f6
Shestakov Matvey <mse25019@gmail.com>
2025-09-04 14:21:39 +0500

    add UserAuthSchema and update UserRegisterSchema

--- saver/src/users/schemas.py
+++ saver/src/users/schemas.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field
 
 
-class UserRegisterSchema(BaseModel):
-    username: str = Field(..., min_length=2, max_length=50, description="Username consisting of 2 to 50 characters")
+class UserAuthSchema(BaseModel):
     email: EmailStr = Field(..., description="User E-mail")
     password: str = Field(..., min_length=6, max_length=50, description="The user's password is from 6 characters to 50")
-    pass
+
+
+class UserRegisterSchema(UserAuthSchema):
+    username: str = Field(..., min_length=2, max_length=50, description="Username consisting of 2 to 50 characters")
 
 
 class ExpenseSchema(BaseModel):
