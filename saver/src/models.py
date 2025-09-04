c6adc3ec1c350281ee44ea96597c6d394c451e14
Shestakov Matvey <mse25019@gmail.com>
2025-09-04 12:13:02 +0500

    update class Base

--- saver/src/models.py
+++ saver/src/models.py
@@ -13,8 +13,8 @@ class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"))
 
 
     @declared_attr.directive
