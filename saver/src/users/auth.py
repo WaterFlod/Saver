9333823ee0a2c12336b72fc2ad00a1ff003bde89
Shestakov Matvey <mse25019@gmail.com>
2025-09-04 18:31:58 +0500

    add get_current_user

--- saver/src/users/auth.py
+++ saver/src/users/auth.py
@@ -1,7 +1,8 @@
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 from pydantic import EmailStr
 from datetime import datetime, timedelta, timezone
+from fastapi import Request, HTTPException, status, Depends
 
 from src.config import settings
 from src.users.dao import UserDAO
@@ -30,4 +31,34 @@ async def authenticate_user(email: EmailStr, password: str):
     user = await UserDAO.find_one_or_none(email=email)
     if not user or verify_password(password_from_user=password, password_in_database=user.password) is False:
         return None
+    return user
+
+
+def get_token(request: Request):
+    token = request.cookies.get("user_access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not found")
+    return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        auth_data = settings.get_auth_data()
+        payload = jwt.decode(token, auth_data["secret_key"], algorithms=[auth_data["algorithm"]])
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="The token is not valid")
+    
+    expire = payload.get("exp")
+    expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+    if (not expire) or (expire_time < datetime.now(timezone.utc)):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="The token has expired")
+    
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User ID not found")
+    
+    user = await UserDAO.find_one_or_none(int(user_id))
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    
     return user
