b7f60dc5da3b74c82418095ddc80c35bfca89615
Shestakov Matvey <mse25019@gmail.com>
2025-09-04 11:10:06 +0500

    add class UserRegisterSchema

--- saver/src/users/schemas.py
+++ saver/src/users/schemas.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
-class Expense(BaseModel):
+
+class UserRegisterSchema(BaseModel):
+    username: str = Field(..., min_length=2, max_length=50, description="Username consisting of 2 to 50 characters")
+    email: EmailStr = Field(..., description="User E-mail")
+    password: str = Field(..., min_length=6, max_length=50, description="The user's password is from 6 characters to 50")
+    pass
+
+
+class ExpenseSchema(BaseModel):
     amount: int
     description: str
